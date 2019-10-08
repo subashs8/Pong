728274e1a63065366ecc8327bc9d5d2bdc7f654f
Alan Liang <Liang59046>
2019-10-08 10:15:11 -0700

    added buttons to game over screens

--- app/src/main/java/com/example/tennis/Gameover.java
+++ app/src/main/java/com/example/tennis/Gameover.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Gameover extends AppCompatActivity {
+public class Gameover extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameover);
+        final Button button = findViewById(R.id.loseRestart);
+        button.setOnClickListener(this);
     }
 
-    public void backToGame(View view){
+    public void onClick(View view){
         Intent i = new Intent(this, Game.class);
         startActivity(i);
     }
--- app/src/main/java/com/example/tennis/Win.java
+++ app/src/main/java/com/example/tennis/Win.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class Win extends AppCompatActivity {
+public class Win extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+        final Button button = findViewById(R.id.winRestart);
+        button.setOnClickListener(this);
     }
 
-    public void backToGame(View view){
+    public void onClick(View view){
         Intent i = new Intent(this, Game.class);
         startActivity(i);
     }
